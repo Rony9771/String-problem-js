db70ac42f5005ec57692fbf8508ad41d525bf14b
Rony <rony.ahmed9771@gmail.com>
2022-08-09 05:33:40 +0600

    String Comparsion

---
+++ case.js
@@ -0,0 +1,17 @@
+const herName = "SundoriKomola";
+const gfName = "Sundorikomola";
+
+console.log(gfName.toLocaleLowerCase);
+
+if (herName.toLowerCase === gfName.toLowerCase){
+  console.log ("She is my jantu");
+}
+else {
+  console.log("I don't know her");
+}
+if (herName === gfName){
+  console.log ("She is my jantu");
+}
+else {
+  console.log("I don't know her");
+}
