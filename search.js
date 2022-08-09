476e8ffcb900e250bac7abfd485e3e3cfe741b95
Rony <rony.ahmed9771@gmail.com>
2022-08-09 06:16:53 +0600

    string search added

---
+++ search.js
@@ -0,0 +1,6 @@
+const name1 = "Akhi Supti Nusrat Munni Mishu Taslima Shikta Ruma";
+const doesExist = name1.includes("Shikta");
+console.log(doesExist);
+//console.log(name1.indexof('shikta'));
+console.log(name1.startsWith("Akhi"));
+console.log(name1.endsWith("Shikta"));
