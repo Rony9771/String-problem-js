721ae0d1f412190727c13d3a9c14cda4801547b3
Rony <rony.ahmed9771@gmail.com>
2022-08-09 06:23:20 +0600

    Search updated

--- search.js
+++ search.js
@@ -4,3 +4,10 @@ console.log(doesExist);
 //console.log(name1.indexof('shikta'));
 console.log(name1.startsWith("Akhi"));
 console.log(name1.endsWith("Shikta"));
+
+
+const file1 = "Research.pdf";
+const file2 = "GetLove.doc"
+
+const file= file1.endsWith(".pdf");
+console.log(file);
